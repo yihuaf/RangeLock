e64a373aa484afbdb62fd4635cf5010c71b01b6f
Eric Fang <efang@stacklighting.com>
2017-09-13 18:12:29 -0700

    Basic range lock test added

--- rangelock/rangelock_test.go
+++ rangelock/rangelock_test.go
@@ -6,8 +6,16 @@ import (
 )
 
 func TestBasic(t *testing.T) {
-	lock := rangelock.RangeLock{}
-	if lock.Lock(3, 35) != nil {
-		t.Error("Failed ..")
+	lock := rangelock.New()
+	if err := lock.Lock(3, 35); err != nil {
+		t.Fatal("Should be able to lock", err)
+	}
+
+	if err := lock.Lock(4, 5); err == nil {
+		t.Fatal("Should not lock")
+	}
+
+	if err := lock.Unlock(3, 35); err != nil {
+		t.Fatal("Should unlock")
 	}
 }
